917738827335ea638259cc9f20d2a3e1c10e36ca
braiancred <110556735+braiancred@users.noreply.github.com>
2022-12-20 18:55:31 -0300

    Delete src/src directory

--- src/src/components/Cart.jsx
+++
@@ -1,9 +0,0 @@
-const Cart = () => {
-    return (
-        <>
-            <h1>I'm Cart 🎯</h1>
-        </>
-    );
-}
-
-export default Cart;
--- src/src/components/CartWidget.jsx
+++
@@ -1,9 +0,0 @@
-import { AiOutlineShoppingCart } from 'react-icons/ai';
-
-const CartWidget = () => {
-    return (
-        <div id="icono"><button><AiOutlineShoppingCart /></button></div>
-    );
-}
-
-export default CartWidget;
