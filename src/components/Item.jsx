bc8808339229b47b0c352ddc1b4a9908d841ae8e
braiancred <110556735+braiancred@users.noreply.github.com>
2022-12-20 04:45:13 -0300

    Add files via upload

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -4,7 +4,7 @@ import "../App.css";
 const Item = ({ banner, id }) => {
     return (
             <div className="card">
-                <img className="cardImagen" src={banner} alt="banner del evento"/>
+                <img src={banner} alt="banner del evento"/>
                 <button className="botonMasInfo"><Link to={`/item/${id}`}>Mostrar info</Link></button>
             </div> 
     );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,18 +29,6 @@ const Navbar = () => {
                     </div>
             </nav> 
         </div>
-        /* <div>
-            <header className="navbar">
-                <div className="titulo"><Link to="/">ELECTRONIC ARG</Link></div>
-                <div className="estilosElectronica">
-                    <div className="estilo"><Link to="/style/Techno">Techno</Link></div>
-                    <div className="estilo"><Link to="/style/Melodic-Techno">Melodic-Techno</Link></div>
-                    <div className="estilo"><Link to="style/Tech-House">Tech-House</Link></div>
-                    <div className="estilo"><Link to="/style/Progressive">Progressive</Link></div>
-                </div>
-                <div id="icono"><button><AiOutlineShoppingCart /></button></div>
-            </header>
-        </div> */
     )
 }
 
